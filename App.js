4ff047683f06900468639fceae956c588fae872f
Jagat Jeeban Maharana <jagatjeebanmaharana@gmail.com>
2024-05-07 23:15:59 +0530

    initial setup

---
+++ App.js
@@ -0,0 +1,13 @@
+import { View, Text, SafeAreaView } from 'react-native';
+import React from 'react';
+import Animated from 'react-native-reanimated';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text style={{color: 'white'}}>App</Text>
+    </SafeAreaView>
+  )
+}
+
+export default App;
