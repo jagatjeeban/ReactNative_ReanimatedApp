d192e2cd9d5ffb670f508cd385c6d840a8dd1202
Jagat Jeeban Maharana <jagatjeebanmaharana@gmail.com>
2024-05-08 00:13:12 +0530

    learn useSharedValue, withSpring and withTiming

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
-import { View, Text, SafeAreaView } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet, TouchableOpacity } from 'react-native';
 import React from 'react';
-import Animated from 'react-native-reanimated';
+import Animated, { useSharedValue, withSpring, withTiming } from 'react-native-reanimated';
 
 const App = () => {
+
+  const animatedValue = useSharedValue(100);  // can pass number, string, boolean, array, objects..
+
   return (
-    <SafeAreaView>
-      <Text style={{color: 'white'}}>App</Text>
+    <SafeAreaView style={styles.safeAreaView}>
+      <Animated.View style={{width: animatedValue, height: animatedValue, backgroundColor:'red'}}>
+      </Animated.View>
+      <TouchableOpacity 
+        onPress={() => {
+          animatedValue.value = withSpring(animatedValue.value + 50)
+        }} 
+        style={{height: 50, marginHorizontal:30, marginTop:"50%", alignItems:'center', justifyContent:"center", backgroundColor:'white'}}
+      >
+        <Text style={{fontSize: 20}}>Animate</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+  }
+})
 
 export default App;
